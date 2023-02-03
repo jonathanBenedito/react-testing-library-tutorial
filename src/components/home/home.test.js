b60f74e0b7f75636143eb26bccb3eea6471df2f8
PatchesTheWright <jonathan.valbe.jb@gmail.com>
2023-02-02 23:17:17 -0300

    User events tutorial

--- src/components/home/home.test.js
+++ src/components/home/home.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event";
 import { Home } from "."
 
 test("inputs should be initially empty", () => {
@@ -11,4 +12,46 @@ test("inputs should be initially empty", () => {
     expect(emailInputElement.value).toBe("");
     expect(passwordInputElement.value).toBe("");
     expect(confirmPasswordInputElement.value).toBe("");
+})
+
+test("should be able to type an email", () => {
+    render(<Home />);
+    const emailInputElement = screen.getByRole("textbox", {
+        name: /email/i
+    });
+    userEvent.type(emailInputElement, "selena@gmail.com");
+    expect(emailInputElement.value).toBe("selena@gmail.com");
+})
+
+test("should be able to type a password", () => {
+    render(<Home />);
+    const passwordInputElement = screen.getByLabelText("Password");
+    userEvent.type(passwordInputElement, "password!");
+    expect(passwordInputElement.value).toBe("password!");
+})
+
+test("should be able to type a confirm password", () => {
+    render(<Home />);
+    const confirmPasswordInputElement = 
+        screen.getByLabelText("Confirm Password");
+    userEvent.type(confirmPasswordInputElement, "password!");
+    expect(confirmPasswordInputElement.value).toBe("password!");
+})
+
+test("should show email error message on invalid email", () => {
+    render(<Home />);
+
+    const emailErrorElement = screen.getByText(/the email you input is invalid/i)
+    const emailInputElement = screen.getByRole("textbox", {
+        name: /email/i,
+    });
+    const submitBtnElement = screen.getByRole("button", {
+        name: /submit/i
+    })
+
+    expect(emailErrorElement).not.toBeInTheDocument();
+
+    userEvent.type(emailInputElement, "selenagmail.com")
+    userEvent.click(submitBtnElement)
+    
 })
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,35 @@
+import { useState } from "react"
+
 export const Home = () => {
+
+    const [signupInput, setSignInput] = useState({
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
+
+    const handleChange = (e) => {
+        setSignInput({
+            ...signupInput,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <form>
             <label for="address">Email address</label>
             <br/>
-            <input type="email" id="address" />
+            <input
+                type="email"
+                id="address"
+                name="email"
+                value={signupInput.email}
+                onChange={handleChange}
+            />
             <br/>
 
             <label for="password">Password</label>
@@ -16,6 +42,8 @@ export const Home = () => {
             <input type="password" id="confirm" />
             <br/>
             <br/>
+
+            <p>the email you input is invalid!</p>
 
             <input type="submit" value="Submit" />
         </form>
